65968e1d69762f513d0242e9be5b2f3d736d9f7c
Dima0o <tudok124@gmail.com>
2018-03-31 12:17:40 +0300

    page add profile

--- app/eshop.js
+++ app/eshop.js
@@ -104,7 +104,7 @@ function PageCatalog(uid) {
         console.log(data);
       }
     });*/
-    console.log(uid);
+   //console.log(uid);
     $.ajax({
         type: "POST",
         dataType: "json",
@@ -112,7 +112,7 @@ function PageCatalog(uid) {
         // url: 'lol.php',
         url: 'dev/profile.php',
         success: function(data) {
-            console.log(data);
+            //console.log(data);
             PageProfiles(data,'#result');
         }
     });
@@ -168,50 +168,51 @@ function Bild_Blok(data,id) {
   $(id).html(items);
 
 }
-function PageProfiles(data,id) {
-  var blok='<div class="row">\n' +
-      '  <div class="col-xs-4 item-photo">\n' +
-      '    <img style="max-width:100%;" id="img" src="'+data.img+'" />\n' +
-      '  </div>\n' +
-      '  <div class="col-xs-5" style="border:0px solid gray">\n' +
-      '    <!-- Datos del vendedor y titulo del producto -->\n' +
-      '    <h3 name="name">'+data.name+'</h3>\n' +
-      '    <!-- Precios -->\n' +
-      '    <h6 class="title-price"><small data-categoru="categoru-tipe">Свежая родукция</small></h6>\n' +
-      '    <h3 style="margin-top:0px;" data-price="price">U$S 399</h3>\n' +
-      '\n' +
-      '    <!-- Detalles especificos del producto -->\n' +
-      '    <div class="section">\n' +
-      '      <h6 class="title-attr" style="margin-top:15px;" ><small>COLOR</small></h6>\n' +
-      '      <div>\n' +
-      '        <div class="attr" style="width:25px;background:#5a5a5a;"></div>\n' +
-      '        <div class="attr" style="width:25px;background:white;"></div>\n' +
-      '      </div>\n' +
-      '    </div>\n' +
-      '    <div class="section" style="padding-bottom:5px;">\n' +
-      '      <h6 class="title-attr"><small>Краткие характеристики</small></h6>\n' +
-      '      <div id="characteristics">\n' +
-      '        <div class="attr2">16 GB</div>\n' +
-      '        <div class="attr2">32 GB</div>\n' +
-      '      </div>\n' +
-      '    </div>\n' +
-      '    <div class="section" style="padding-bottom:20px;">\n' +
-      '      <h6 class="title-attr"><small>Количество</small></h6>\n' +
-      '      <div>\n' +
-      '\n' +
-      '      </div>\n' +
-      '    </div>\n' +
-      '\n' +
-      '    <!-- Botones de compra -->\n' +
-      '    <div class="section" style="padding-bottom:20px;">\n' +
-      '      <button class="btn btn-success"><span style="margin-right:20px" class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>В корзину</button>\n' +
-      '      <h6><a href="#"><span class="glyphicon glyphicon-heart-empty" style="cursor:pointer;"></span>*****Сравнение ******</a></h6>\n' +
-      '    </div>\n' +
-      '  </div>\n' +
-      '\n' +
-      '  <div class="col-xs-9">\n' +
-      '    <h6>Описание товаров</h6>\n' +
-      '  </div>\n' +
-      '</div>';
+function PageProfiles(dat,id) {
+  var data=dat[0];
+    var blok='<div class="row">\n' +
+        '  <div class="col-xs-4 item-photo">\n' +
+        '    <img style="max-width:100%;" id="img" src="'+data.img+'" />\n' +
+        '  </div>\n' +
+        '  <div class="col-xs-5" style="border:0px solid gray">\n' +
+        '    <!-- Datos del vendedor y titulo del producto -->\n' +
+        '    <h3 name="name">'+data.name+'</h3>\n' +
+        '    <!-- Precios -->\n' +
+        '    <h6 class="title-price"><small data-categoru="categoru-tipe">Свежая родукция</small></h6>\n' +
+        '    <h3 style="margin-top:0px;" data-price="price">U$S 399</h3>\n' +
+        '\n' +
+        '    <!-- Detalles especificos del producto -->\n' +
+        '    <div class="section">\n' +
+        '      <h6 class="title-attr" style="margin-top:15px;" ><small>COLOR</small></h6>\n' +
+        '      <div>\n' +
+        '        <div class="attr" style="width:25px;background:#5a5a5a;"></div>\n' +
+        '        <div class="attr" style="width:25px;background:white;"></div>\n' +
+        '      </div>\n' +
+        '    </div>\n' +
+        '    <div class="section" style="padding-bottom:5px;">\n' +
+        '      <h6 class="title-attr"><small>Краткие характеристики</small></h6>\n' +
+        '      <div id="characteristics">\n' +
+        '        <div class="attr2">16 GB</div>\n' +
+        '        <div class="attr2">32 GB</div>\n' +
+        '      </div>\n' +
+        '    </div>\n' +
+        '    <div class="section" style="padding-bottom:20px;">\n' +
+        '      <h6 class="title-attr"><small>Количество</small></h6>\n' +
+        '      <div>\n' +
+        '\n' +
+        '      </div>\n' +
+        '    </div>\n' +
+        '\n' +
+        '    <!-- Botones de compra -->\n' +
+        '    <div class="section" style="padding-bottom:20px;">\n' +
+        '      <button class="btn btn-success"><span style="margin-right:20px" class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span>В корзину</button>\n' +
+        '      <h6><a href="#"><span class="glyphicon glyphicon-heart-empty" style="cursor:pointer;"></span>*****Сравнение ******</a></h6>\n' +
+        '    </div>\n' +
+        '  </div>\n' +
+        '\n' +
+        '  <div class="col-xs-9">\n' +
+        '    <h6>Описание товаров</h6>\n' +
+        '  </div>\n' +
+        '</div>';
     $(id).html(blok);
 }
